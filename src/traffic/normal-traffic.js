3396d5c33a2bc34f62de876c2d3eeac9c5b4b492
adelethorberg-sudo <adele.thorberg@gmail.com>
2026-09-26 15:25:11 +0200

    simulerer trafikk for login, kjøp og browsing

--- src/traffic/normal-traffic.js
+++ src/traffic/normal-traffic.js
@@ -1,17 +1,80 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-export const options = {
-  stages: [
-    { duration: '10s', target: 5 },
-    { duration: '30s', target: 10 },
-    { duration: '10s', target: 0 },
-  ],
-};
-
 const baseUrl = 'http://host.docker.internal:5000';
 
-export default function () {
+const USERS = [
+  { username: 'alice', password: 'alice123'},
+  { username: 'bob', password: 'bob123' },
+  ...Array.from({ length: 20 }, (_, i) => ({
+    username: `bruker${i + 1}`,
+    password: `Passord${i + 1}!`,
+  })),
+]
+
+const PRODUCTS = [
+  { id: 1, name: 'Eplejuice', price: '39.0' },
+  { id: 2, name: 'Appelsinjuice', price: '45.0' },
+  { id: 3, name: 'Bærmix', price: '55.0' },
+  { id: 4, name: 'VIP Gullpakke', price: '999.0' },
+];
+
+//brukere vi vet får logget inn
+const VALID_USERS = [
+  { username: 'alice', password: 'alice123'},
+  { username: 'bob', password: 'bob123' },
+]
+
+export const options = {
+  scenarios: {
+    login: {
+      executor: 'shared-iterations',
+      exec: 'login',
+      vus: 10,
+      iterations: 50,
+      startTime: '0s',
+    },
+
+    buyProduct: {
+      executor: 'shared-iterations',
+      exec: 'buyProduct',
+      vus: 10,
+      iterations: 40,
+      startTime: '10s',
+    },
+
+    browse: {
+      executor: 'shared-iterations',
+      exec: 'browse',
+      vus: 10,
+      iterations: 100,
+      startTime: '5s',
+    },
+  },
+}
+
+export function login(){
+  const user = USERS[Math.floor(Math.random()* USERS.length)]
+
+  const res = http.post(baseUrl + '/login', {username: user.username, password: user.password}, {redirects: 0});
+
+  check(res, { 'login OK': (r) => r.status === 302 });
+}
+
+export function buyProduct(){
+  const user = VALID_USERS[Math.floor(Math.random()* VALID_USERS.length)]
+  const product = PRODUCTS[Math.floor(Math.random() * PRODUCTS.length)];
+
+  http.post(baseUrl + '/login', {username: user.username, password: user.password}, {redirects: 0});
+
+  sleep(1);
+
+  const res = http.post(`${baseUrl}/checkout`, { product_name: product.name, price: product.price, });
+ 
+  check(res, { 'kjøp OK': (r) => r.status === 200 });
+}
+
+export function browse(){
   const choice = Math.random();
 
   let path;
@@ -32,7 +95,7 @@ export default function () {
   const response = http.get(`${baseUrl}${path}`);
 
   check(response, {
-    'status is 200': (response) => response.status === 200,
+    'browse ok': (response) => response.status === 200,
   });
 
   sleep(Math.random() * 3 + 1);
